0392f7e6afb72c6adc04db1bad901ed0a386c796
Gabriel Pillajo <gpillajo14@gmail.com>
2024-11-09 11:31:52 -0500

    merge in develop cambio user path

--- app/routes/user.router.js
+++ app/routes/user.router.js
@@ -1,14 +1,9 @@
 import express from "express";
-import {
-  saveUser,
-  getUsers,
-  deleteUser,
-} from "../controllers/user.controller.js";
+import { saveUser, sendWelcomeEmail } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.post("/", saveUser);
-router.get("/all", getUsers);
-router.delete("/:id", deleteUser);
-
+router.post("/send-welcome-email", sendWelcomeEmail);
+router.post("/forgot-password");
 export default router;
